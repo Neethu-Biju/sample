b96c7e3bae02f487c5bf94ea3c6341112a5436d1
Neethu <st.thomas2426@gmail.com>
2026-05-05 12:47:30 +0530

    product of two numbers

--- app.py
+++ app.py
@@ -2,3 +2,5 @@ a = int(input("Enter first number: "))
 b = int(input("Enter second number: "))
 c = a+b
 print(f"the sum of {a} and {b} is : {c}")
+c = a * b
+print(f"the product of {a} and {b} is :{c}")
